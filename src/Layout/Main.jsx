ef5cac0049a3e22f2126cbb17c414098569b963c
SalehinShohan <ps.shohan1@gmail.com>
2023-06-07 14:39:17 +0600

    add footer section

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,12 +1,15 @@
 import { Outlet } from "react-router-dom";
-
+import Footer from "../Shared/Footer";
 
 const Main = () => {
-    return (
-        <div>
-            <Outlet></Outlet>
-        </div>
-    );
+  return (
+    <div>
+      <div className="min-h-[calc(100vh-137px)]">
+        <Outlet></Outlet>
+      </div>
+      <Footer></Footer>
+    </div>
+  );
 };
 
 export default Main;
